ed78411e5e6a31dcd8804c6db9badc3101b9ef75
Deependra-Negi <916deepaknegi@gmail.com>
2022-03-12 16:39:28 +0530

    initial commit

---
+++ src/pages/ChatPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChatWindow from '../components/ChatWindow'
+import Header from '../components/Header'
+
+function ChatPage() {
+  return (
+    <>
+        <Header />
+        <ChatWindow/>
+    </>
+  )
+}
+
+export default ChatPage
